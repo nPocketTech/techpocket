1eaf2a32e91631063e7c2bd92c155f95b2a60471
Xunhaoz <leo20020529@gmail.com>
2023-11-26 11:03:39 +0800

    Update: sport

--- example.py
+++ example.py
@@ -1,14 +1,22 @@
 from pprint import pprint
-
 from techpocket.core import TechPocket
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('max_colwidth', 500)
 
 tp = TechPocket('DAC8D4453347487F24CDFCE061DF089FA1ED786B')
 print('balance:', tp.get_balance())
 
-pprint(tp.sport.get_sport_scores(sport='NBA', date='2023-01-17'))
-pprint(tp.sport.get_sport_odds(sport='NBA', date='2023-01-17'))
-pprint(tp.sport.get_sport_lineups(sport='NBA'))
+# pprint(tp.sport.get_sport_scores(sport='NBA', date='2023-01-17', df_like=False, save_to="./check/check.csv"))
+# pprint(tp.sport.get_sport_scores(sport='NBA', date='2023-01-17', df_like=True, save_to="./check/check.csv"))
+# pprint(tp.sport.get_sport_odds(sport='NBA', date='2023-01-17', df_like=False, save_to="check.csv"))
+# pprint(tp.sport.get_sport_odds(sport='NBA', date='2023-01-17', df_like=True, save_to="./check"))
+# pprint(tp.sport.get_sport_odds(sport='NBA', date='2023-01-17', df_like=True, save_to="./check/check.csv"))
+# pprint(tp.sport.get_sport_lineups(sport='NBA', df_like=False, save_to='./check/check.csv'))
+# pprint(tp.sport.get_sport_lineups(sport='NBA', df_like=True, save_to='./check'))
+# pprint(tp.sport.get_sport_lineups(sport='NBA', df_like=True, save_to='./check/check.csv'))
 
-pprint(tp.stock.get_list(key='興櫃股票'))
-pprint(tp.stock.get_ticks_realtime(stock_id='TSE'))
-pprint(tp.stock.get_price_realtime(stock_list=['2330', '2454']))
+# pprint(tp.stock.get_list(key='興櫃股票'))
+# pprint(tp.stock.get_ticks_realtime(stock_id='TSE'))
+# pprint(tp.stock.get_price_realtime(stock_list=['2330', '2454']))
--- techpocket/service/sport.py
+++ techpocket/service/sport.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
+import pathlib
+from pathlib import Path
+from datetime import date as date_library
+
+import pandas as pd
+from dateutil import parser
+
+
 class Sport:
     def __init__(self, _request):
         self._request = _request
+        self._today = date_library.today().strftime("%Y-%m-%d")
 
-    def get_sport_scores(self, sport: str, date: str) -> dict:
+    def get_sport_scores(self, sport: str, date: str = "", df_like=False, save_to: str = "") -> dict or pd.DataFrame:
         '''
         [Parameters]
         ------------
@@ -13,35 +23,23 @@ class Sport:
 
         [Returns]
         ------------
-        return: list[dict]
-            date: str,
-                YYYY-MM-DD
-            time: str,
-                hh:mm
-            status: str,
-                 'Playing', 'Final'
-            away: dict,
-                name: str,
-                score: int,
-                box: list,
-                hit: int (basketball),
-                error: int (basketball)
-            home: dict,
-                name: str,
-                score: int,
-                box: list,
-                hit: int (baseball),
-                error: int (baseball)
+        games: 詳情請上 https://npocket.tech/doc/sport/api#%E5%8D%B3%E6%99%82%E6%AF%94%E5%88%86
         '''
-        res = self._request('sport/scores', 0, sport=sport, date=date)
+        self._handle_path(save_to)
+        date = self._parse_user_input(date) if date else self._today
 
-        if res['status']['code'] == 200:
-            res.pop('status', None)
-            return res
+        games = self._request('sport/scores', sport=sport, date=date)['games']
 
-        raise Exception(res['status']['msg'])
+        if df_like:
+            games = pd.json_normalize(games)
 
-    def get_sport_odds(self, sport: str, date: str) -> dict:
+            games = games.rename(columns={col: col.replace('.', '_') for col in games.columns})
+            if save_to:
+                games.to_csv(save_to)
+
+        return games
+
+    def get_sport_odds(self, sport: str, date: str, df_like=False, save_to: str = "") -> dict or pd.DataFrame:
         '''
         [Parameters]
         ------------
@@ -52,81 +50,119 @@ class Sport:
 
         [Returns]
         ------------
-        return: list[dict]
-            date: str,
-                YYYY-MM-DD
-            time: str,
-                hh:mm
-            lottery_id: str,
-            away: str,
-            home: str,
-            odds: dict,
-                away_is_plus: int,
-                away_handi: int,
-                away_normal: int,
-                home_handi: int,
-                home_normal: int,
-                total: int,
-                big: int,
-                small: int,
-                update_time: str
-                handi: int (basketball, tennis)
-                big_15：int (soccer, tennis, hockey),
-                big_25：int (soccer, tennis, hockey),
-                big_35：int (soccer, tennis, hockey),
-                small_15：int (soccer, tennis, hockey),
-                small_25：int (soccer, tennis, hockey),
-                small_35：int (soccer, tennis, hockey),
-                num_024：int (soccer, tennis, hockey),
-                num_135：int (soccer, tennis, hockey),
-                tie_handi：int (soccer, tennis, hockey),
-                tie_normal: int (soccer, tennis, hockey)
+        games: 詳情請上 https://npocket.tech/doc/sport/api#%E8%B3%A0%E7%8E%87%E8%A8%98%E9%8C%84
         '''
-        res = self._request('sport/odds', 0, sport=sport, date=date)
 
-        if res['status']['code'] == 200:
-            res.pop('status', None)
-            return res
+        date = self._parse_user_input(date) if date else self._today
 
-        raise Exception(res['status']['msg'])
+        games = self._request('sport/odds', sport=sport, date=date)['games']
 
-    def get_sport_lineups(self, sport) -> dict:
+        if df_like:
+            games = self._normalized_save_sport_odds(games, save_to)
+
+        return games
+
+    def get_sport_lineups(self, sport: str, df_like=False, save_to: str = "") -> dict or pd.DataFrame:
         '''
         [Parameters]
         ------------
         sport: str
-            'NBA'
-
         [Returns]
         ------------
-        return: list[dict]
-            date: str,
-                YYYY-MM-DD
-            time: str,
-                hh:mm
-            away: dict,
-                name: str,
-                starting: dict,
-                    name：str,
-                    pos: str
-                injuries: dict:
-                    name: str,
-                    pos: str,
-                    status: str
-            home: dict,
-                name: str,
-                starting: dict,
-                    name：str,
-                    pos: str
-                injuries: dict:
-                    name: str,
-                    pos: str,
-                    status: str
+        games: 詳情請上 https://npocket.tech/doc/sport/api#%E8%B3%BD%E4%BA%8B%E5%90%8D%E5%96%AE
         '''
-        res = self._request('sport/lineups', 0, sport=sport)
+        games = self._request('sport/lineups', sport=sport)['games']
 
-        if res['status']['code'] == 200:
-            res.pop('status', None)
-            return res
+        if df_like:
+            games = self._normalized_save_sport_lineups(games, save_to)
 
-        raise Exception(res['status']['msg'])
+        return games
+
+    def _normalized_save_sport_odds(self, games: dict, save_to: str = '') -> pd.DataFrame:
+        path_obj = self._handle_path(save_to)
+
+        res = []
+        for game in games:
+            odds = pd.DataFrame(game['odds'])
+            odds['home'] = game['home']
+            odds['away'] = game['away']
+            odds['date'] = game['date']
+            odds['lottery_id'] = lottery_id = game['lottery_id']
+            csv_name = lottery_id + '.csv'
+
+            if save_to and path_obj.is_dir():
+                odds.to_csv(path_obj / csv_name)
+            res.append(odds)
+
+        games = pd.concat(res, axis=0, ignore_index=True)
+        if save_to and path_obj.is_file():
+            games.to_csv(save_to)
+
+        return games
+
+    def _normalized_save_sport_lineups(self, games: dict, save_to: str = '') -> pd.DataFrame:
+        path_obj = self._handle_path(save_to)
+
+        # 若路徑為資料夾，則分檔案創建
+        # 若路徑為資料夾，則一次儲存
+        res = []
+        for game in games:
+            away_name = game['away']['name']
+            home_name = game['home']['name']
+            csv_name = f"{away_name}-{home_name}-{game['date']}-{game['time']}.csv".replace(':', '')
+
+            away = self._normalize_starting_injuries(game, 'away')
+            home = self._normalize_starting_injuries(game, 'home')
+
+            df = pd.concat([home, away])
+            df['date'] = game['date']
+            df['time'] = game['time']
+
+            if save_to and path_obj.is_dir():
+                df.to_csv(path_obj / csv_name)
+            res.append(df)
+
+        games = pd.concat(res, axis=0, ignore_index=True)
+        if save_to and path_obj.is_file():
+            games.to_csv(save_to)
+
+        return games
+
+    @staticmethod
+    def _parse_user_input(user_input):
+        parsed_date = parser.parse(user_input).strftime("%Y-%m-%d")
+        return parsed_date
+
+    @staticmethod
+    def _normalize_starting_injuries(game, home_away: str):
+        first_data = game[home_away]
+        name = first_data['name']
+
+        starting, starting_len = first_data['starting'], len(first_data['starting'])
+        starting = pd.DataFrame(starting)
+        starting['status_detail'] = None
+        starting['status'] = ['starting'] * starting_len
+
+        injuries, injuries_len = first_data['injuries'], len(first_data['injuries'])
+        injuries = pd.DataFrame(injuries)
+        injuries = injuries.rename(columns={'status': 'status_detail'})
+        injuries['status'] = ['injuries'] * injuries_len
+
+        normalized = pd.concat([starting, injuries], axis=0, ignore_index=True)
+        normalized['team'] = [name] * len(normalized)
+
+        return normalized
+
+    @staticmethod
+    def _handle_path(save_to: str) -> pathlib.Path:
+        path_obj = Path(save_to)
+
+        # 如果不存在則創建父資料夾路徑
+        if path_obj != '':
+            if path_obj.suffix:
+                path_obj.parent.mkdir(parents=True, exist_ok=True)
+                open(path_obj, 'w').close()
+            else:
+                path_obj.mkdir(parents=True, exist_ok=True)
+
+        return path_obj
